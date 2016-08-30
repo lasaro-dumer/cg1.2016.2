c8caeb7a8ada96acec08fd2864918bb0ea548c2c
Lásaro Dumer <ladumer@hotmail.com>
2016-08-30 06:58:57 -0300

    CSS to public files and bower

--- installBowerComponents.sh
+++ installBowerComponents.sh
@@ -3,3 +3,4 @@ cp bower_components/jquery/dist/jquery.min.js public/js/jquery.min.js
 cp bower_components/bootstrap/dist/js/bootstrap.min.js public/js/bootstrap.min.js
 cp bower_components/angular/angular.min.js public/js/angular.min.js
 cp bower_components/angular-route/angular-route.min.js public/js/angular-route.min.js
+cp bower_components/bootstrap/dist/css/bootstrap.min.css public/css/bootstrap.min.css
