9061044aa05f7cc24bd9060eae1f65bf7c2e71ff
Lásaro Dumer <ladumer@hotmail.com>
2016-09-05 21:15:53 -0300

    First analysis snippet Parse line string functions added

---
+++ t1_analysis/src/t1_analysis/CordElement.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package t1_analysis;
+
+/**
+ *
+ * @author lasaro
+ */
+public class CordElement {
+    public int X;
+    public int Y;
+    public int T;
+
+    @Override
+    public String toString() {
+        return "("+X+","+Y+","+T+")";
+    }
+}
